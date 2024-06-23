d61d946889ef31d7bce394687694a51e29e28ab5
Kevin <kevin@eaucole.com>
2024-06-23 18:55:43 +0800

    Initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1 class="rank"> {{ rank }} </h1>
+    <p>Reaction time {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+    props: ["score"],
+    data() {
+        return {
+            rank: null
+        }
+    },
+
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "🥷 Ninja Fingers";
+        } 
+        else if (this.score < 400) {
+            this.rank = "⚡ Rapid Reflexes";
+        }
+        else {
+            this.rank = "🐌 Snail pace";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
